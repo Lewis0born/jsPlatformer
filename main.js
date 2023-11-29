74c544e2181725e6e609eae242d3a8c439792566
Lewis0born <lewis.oborn@gmail.com>
2023-11-29 13:38:14 +1030

    Create new folder for js classes
    Move Player class to classes folder.

--- main.js
+++ main.js
@@ -7,37 +7,7 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-// Player class
-class Player {
-    // Player properties (position/width/height)
-    constructor() {
-        this.position = {
-            x: 100,
-            y: 100
-        }
-
-        this.width = 100;
-        this.height = 100;
-        this.sides = {
-            bottom: this.position.y + this.height
-        }
-    }
-
-    // draw player (red rect for now)
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-
-    // Properties to change/update
-    update() {
-        // GRAVITY, increase player Y position until bottom == canvas height
-        if (this.sides.bottom < canvas.height){
-            this.position.y++;
-            this.sides.bottom = this.position.y + this.height;
-        }
-    }
-}
+// Create player object
 const player = new Player();
 
 // animation loop
